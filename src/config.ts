1ff9a0803adf09974f173aaa975d31262fbeed0c
Joel Soderstrom <joel.soderstrom@gmail.com>
2021-09-09 13:53:16 +0200

    Adds defaul val for config

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 const config = {
-    "title" : process.env.REACT_APP_APPNAME,
-    "api" : process.env.APIURL
+    "title" : process.env.REACT_APP_APPNAME || "Admin",
+    "api" : process.env.APIURL || "http://localhost:3333/managementapi"
 };
 export default config;
